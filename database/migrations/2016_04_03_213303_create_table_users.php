3ddd290f73f784c699dfb77a3d0b50381caa004f
Lukasz Fokin <lukasz.fokin@gmail.com>
2016-04-04 13:28:24 +0000

    migration updated

--- database/migrations/2016_04_03_213303_create_table_users.php
+++ database/migrations/2016_04_03_213303_create_table_users.php
@@ -3,7 +3,7 @@
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class CreateTableUsuario extends Migration
+class CreateTableUsers extends Migration
 {
     /**
      * Run the migrations.
