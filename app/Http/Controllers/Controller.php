daa137eb10cc192ec99020a0707ddc7bea615afa
Lukasz Fokin <lukasz.fokin@gmail.com>
2016-04-05 12:46:55 +0000

    Dashboard screen

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace App\Http\Controllers;
+namespace App\Base\Controllers;
 
 use Illuminate\Foundation\Bus\DispatchesJobs;
 use Illuminate\Routing\Controller as BaseController;
@@ -11,4 +11,22 @@ use Illuminate\Foundation\Auth\Access\AuthorizesResources;
 class Controller extends BaseController
 {
     use AuthorizesRequests, AuthorizesResources, DispatchesJobs, ValidatesRequests;
+
+    /**
+     * Var to define if access to controller is restricted by authenticated user
+     * 
+     * @var bool
+     */
+    protected $restricted = true;
+
+    /**
+     * Create a new controller instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        if($this->restricted)
+            $this->middleware('auth');
+    }
 }
