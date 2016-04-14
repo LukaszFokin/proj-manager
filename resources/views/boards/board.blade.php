74ec2f520ea9687fbed33b1cd4dcb332d91ca4e5
Jonas Thomaz de Faria <jonasthomaz@gmail.com>
2016-04-14 17:02:48 -0300

    Dialog
    Openboard dialog.

--- resources/views/boards/board.blade.php
+++ resources/views/boards/board.blade.php
@@ -1,4 +1,11 @@
-<div class="box box-warning box-solid box-board item-drag">
+<?php
+/**
+ * Bloco de Boards 
+ * Trabalhando com array antes de definir a modelagem do banco de dados
+ * @todo Mudar o formato para objeto quando estiver concluido o db
+ */
+?>
+<div class="box box-warning box-solid box-board item-drag " boardid="{{$board['id']}}">
 	<div class="box-header with-border">
     	<h3 class="box-title">{{$board['title']}}</h3>
 		<div class="box-tools pull-right">
@@ -6,4 +13,22 @@
 		</div>
 	</div>
 	<div class="box-body">Texto referente à {{ $board['title'] }}</div>
+</div>
+
+<div id='board{{$board['id']}}' class="modal fade bs-example-modal-lg" tabindex="-1" role="dialog" aria-labelledby="myLargeModalLabel">
+  <div class="modal-dialog modal-lg">
+    <div class="modal-content">
+		<div class="modal-header">
+	        <button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
+	        <h4 class="modal-title" id="exampleModalLabel">{{ $board['title'] }}</h4>
+		</div>
+		<div class="modal-body">
+			asdasdsdas
+		</div>
+		<div class="modal-footer">
+        	<button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
+        	<button type="button" class="btn btn-primary">Save changes</button>
+		</div>
+    </div>
+  </div>
 </div>
--- resources/views/boards/home.blade.php
+++ resources/views/boards/home.blade.php
@@ -4,7 +4,6 @@
 
 {{Html::style('css/boards.css')}}
 
-
 @section('content')
 <div class="box">
 	<div class="box-body">	
@@ -16,14 +15,15 @@
 	    </div>
 	    <div class="row">
 	        <div class="col-md-3 coluna">
-	        	@each('boards.board', array(array('title'=>'Primeira Board'),array('title'=>'Segunda Board')), 'board')
+	        	@each('boards.board', array(array('id'=>'1','title'=>'Primeira Board'),array('id'=>'2','title'=>'Segunda Board')), 'board')
+
 	        </div>
 	        <div class="col-md-3  coluna"></div>
 	        <div class="col-md-3  coluna">
-	        	@each('boards.board', array(array('title'=>'Terceira Board')), 'board')
+	        	@each('boards.board', array(array('id'=>'3','title'=>'Terceira Board')), 'board')
 	        </div>
 	        <div class="col-md-3  coluna">
-	        	@each('boards.board', array(array('title'=>'Quarta Board')), 'board')
+	        	@each('boards.board', array(array('id'=>'4','title'=>'Quarta Board')), 'board')
 	        </div>
 	    </div>
 	</div>
@@ -36,8 +36,8 @@
 <script src="{{ asset('//code.jquery.com/ui/1.11.2/jquery-ui.js') }}"></script>
 <script type="text/javascript">
 $(document).ready(function() {
-    $(".box-board").click(function() {
-  		console.log("eu cliquei em uma board!!");
+    $(".box-board").dblclick(function() {
+  		$('#board'+$(this).attr('boardid')).modal()
 	});
 	$(".coluna").sortable(
 		{
