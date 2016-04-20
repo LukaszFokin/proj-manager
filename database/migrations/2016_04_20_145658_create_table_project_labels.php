f136d3178d7d3d9b6094fe0cc6d13674d1b41f70
Jonas Thomaz de Faria <jonasthomaz@gmail.com>
2016-04-20 12:04:40 -0300

    Migrate
    Criação da tabela project_labels

---
+++ database/migrations/2016_04_20_145658_create_table_project_labels.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateTableProjectLabels extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('project_labels', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('name', 100);
+            $table->string('color',10);
+            $table->integer('project_id')->unsigned()->nullable();
+
+            $table->foreign('project_id')->references('id')->on('projects');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::drop('project_labels');
+    }
+}
